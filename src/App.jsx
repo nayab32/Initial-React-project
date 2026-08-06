1c79a75760d8c4dc1a5a1042ed88fec677a8c4ee
Sheraz khan <nayabiqra32@gmail.com>
2026-08-06 14:29:52 +0500

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App(){
+  return (
+    <div>
+      <h1>First Component</h1>
+      <Fruit>Apple</Fruit>
+    </div>
+  )
+}
+
+function Fruit(){
+  return (
+    <h1>Apple</h1>
+  )
+}
+export default App;
