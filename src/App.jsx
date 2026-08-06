40c8e2e5e1b3d4ece1fa417bcada3b92e6a4c533
Sheraz khan <nayabiqra32@gmail.com>
2026-08-06 16:25:46 +0500

    react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 
+// import Login,{Profile,Setting,UserKey} from "./User";
+
 
 function App(){
+  const userName="iqra";
+  let x= 30;
+  let y=20;
+
   return (
     <div>
-      <h1>First Component</h1>
+      {/* /* <h1>First Component</h1>
+      <Login />
+      <Profile />
+      <Setting />
       <Fruit>Apple</Fruit>
+      // <h1>{UserKey}</h1> */ }
+      <h1>{userName}</h1>
+      <h1>{10+45+55}</h1>
+      <h1>{x+y}</h1>
+
+      
     </div>
   )
 }
 
-function Fruit(){
-  return (
-    <h1>Apple</h1>
-  )
-}
+// function Fruit(){
+//   return (
+//     <h1>Apple</h1>
+//   )
+// }
 export default App;
--- src/User.jsx
+++ src/User.jsx
@@ -1,5 +1,33 @@
-function User(){
-    return <h1>user component</h1>
+// function User(){
+//     return <h1>user component</h1>
     
+// }
+// export default User;
+
+function Login(){
+    return(
+        <div>
+            <h1>Login User</h1>
+        </div>
+    )
 }
-export default User;
+
+export function Profile(){
+    return(
+       <div>
+        <h1>Profile</h1>
+
+        </div>
+    )
+}
+
+export function Setting(){
+    return(
+        <div>
+        <h1>Setting</h1>
+        </div>
+    )
+}
+
+ export const UserKey="@$%^%^#"
+export default Login
