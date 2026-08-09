78899daebf88495cb0508af2726a2f805a513383
Sheraz khan <nayabiqra32@gmail.com>
2026-08-09 19:52:45 +0500

    state

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,8 +1,16 @@
 
+import { useState } from "react";
+
 const Counter=()=>{
+    const[Count,setCount]=useState(0);
+    const [rCounter,setrCounter]=useState(10)
     return (
         <div>
-            <h1>Counter</h1>
+            <h1>Counter:{Count}</h1>
+            <h1>r Counter : {rCounter}</h1>
+            <button onClick={()=> setCount(Count+1)}>Update Counter</button>
+
+            <button onClick={()=> setrCounter(rCounter-1)}>Update rCounter</button>
         </div>
     );
 };
